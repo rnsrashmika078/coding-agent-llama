be33f8593978f06734d41472bc68cd9b70bc3794
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-05-27 01:34:21 +0530

    models

---
+++ model.py
@@ -0,0 +1,10 @@
+from typing import TypedDict, Annotated
+
+from langchain.agents import AgentState
+from langchain_core.messages import BaseMessage
+from langgraph.graph import add_messages
+
+class CustomState(AgentState):
+    messages: Annotated[list[BaseMessage], add_messages]
+    rootPath: str
+    fileTree: str
